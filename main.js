5b3dc27bbe9d5df02521dd1423e5bfe8341edf60
zzraawashere <106053381+zzraawashere@users.noreply.github.com>
2023-01-31 20:05:39 -0700

    works

--- main.js
+++ main.js
@@ -1,26 +1,48 @@
-var cords = document.getElementById("cords");
+var stats = document.getElementById("stats")
+var city = document.getElementById("city");
+var weather = document.getElementById("weather");
+var temp = document.getElementById("temp");
 var snow = document.getElementById("snow");
 
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        cords.innerHTML = "Geolocation is not supported by this browser.";
-    }
+    } 
 }
 
 function showPosition(position) {
-    cords.innerHTML = "Latitude: " + position.coords.latitude +
-        "<br>Longitude: " + position.coords.longitude;
+    
     getWeather(position);
     
 }
 
 function getWeather(position) {
 
-    fetch("api.openweathermap.org/data/2.5/forecast?lat=44&lon=22&appid=d8cfbd2a75723eb46785b6b49b5cbde9")
+    fetch("https://api.openweathermap.org/data/2.5/forecast?lat="+position.coords.latitude+"&lon="+position.coords.longitude+"&units=imperial&appid=d8cfbd2a75723eb46785b6b49b5cbde9")
         .then(response => response.json())
-        .then(data => console.log(data))
+        
+        .then(data => {
+            var cityText = data["city"]["name"]
+            var weatherText = data["list"]["0"]["weather"]["0"]["description"]
+            var tempText = data["list"]["0"]["main"]["temp"]
+
+            stats.innerHTML = "Stats for tomorrow:"
+
+            if (weatherText.includes("snow")) {
+                
+                snow.innerHTML = "Snow day?: ❄️The signs are in your favor! It might be a snow-day tomorrow!❄️"
+                
+            } else {
+                
+                snow.innerHTML = "Snow day?: doesn't look like it :( sorry";
+            }
+
+            
+            city.innerHTML = "City: " + cityText;
+            weather.innerHTML = "Weather: " + weatherText;
+            temp.innerHTML = "Temperature: " + tempText + " °F";
+            
+        })
 
         .catch(err => alert("Invalid Coords!"))
 
